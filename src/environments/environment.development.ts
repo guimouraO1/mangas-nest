fe6b38363eb5dc2c592ff9d72e92d78dc2cb70fd
Guilherme de Moura Oliveira <guimoura.oliveira@gmail.com>
2025-04-29 23:26:36 -0300

    v1.0.0

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // production: false,
-    // apiUrl: 'https://wallet-wise-api-vercel.vercel.app',
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://mangas-nest-api.vercel.app',
+    // apiUrl: 'http://localhost:3000',
     version: '1.0.0'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    // production: true,
-    // apiUrl: 'https://wallet-wise-api-vercel.vercel.app',
-    apiUrl: 'http://localhost:3000',
+    production: true,
+    apiUrl: 'https://mangas-nest-api.vercel.app',
+    // apiUrl: 'http://localhost:3000',
     version: '1.0.0'
 };
